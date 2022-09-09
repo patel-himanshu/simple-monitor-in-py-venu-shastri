f158e9ceb5ea49ee5a4c2d0540dcb904cf5a2b20
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-09-09 12:11:24 +0530

    Refactored code to reduce CCN to 2

--- check_limits.py
+++ check_limits.py
@@ -1,17 +1,32 @@
+def print_message(message): # Returns void
+    print(message)
 
-def battery_is_ok(temperature, soc, charge_rate):
+def is_temperature_ok(temperature):
   if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
     return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False 
-
   return True
 
+def is_soc_ok(soc):
+  if soc < 20 or soc > 80:
+    return False
+  return True
+
+def is_charge_rate_ok(charge_rate):
+  return False charge_rate > 0.8 else True
+
+def battery_is_ok(temperature, soc, charge_rate):
+  messages = [
+    'Temperature is out of range!',
+    'State of Charge is out of range!',
+    'Charge rate is out of range!'
+    ]
+  temperature_check = is_temperature_ok(temperature) 
+  soc_check = is_soc_ok(soc)
+  charge_rate_check = is_charge_rate_ok(charge_rate)
+  
+  if temperature_check and soc_check and charge_rate_check:
+    return True
+  return False 
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
